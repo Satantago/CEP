d1125092b8075faa232dd8f750d99c7923fe7b4d
Mohamed Errazki <mohamed.errazki@grenoble-inp.org>
2022-04-17 22:40:24 +0200

    ajout

--- program/autotest/jalr.s
+++ program/autotest/jalr.s
@@ -1,10 +1,10 @@
-# TAG = jalr
+# TAG = JALR
 	.text
-	
-	li x31,  0
-	
-	
+	li x10 , 0x1012
+	jalr x31, 0(x10)
+	li x31, 8
+	li x31 , 9
 	# max_cycle 50
 	# pout_start
-	# 00000000
+	# 00000009
 	# pout_end
