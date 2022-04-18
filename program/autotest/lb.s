e5583e4904bc5a3c64b0007a2b42a5dd6a27cf2a
LACHIRI <ilias.lachiri@grenoble-inp.org>
2022-04-18 15:37:36 +0200

    ajout

--- program/autotest/lb.s
+++ program/autotest/lb.s
@@ -1,11 +1,12 @@
 # TAG = lb
 	.text
-	
+	lui x11, 6
 	lui x14 , 1
 	lb x31, 0(x14)
 	
 	
 	# max_cycle 50
 	# pout_start
-	# 00000037
+	# 000000B7
 	# pout_end
+	
--- program/autotest/lh.s
+++ program/autotest/lh.s
@@ -1,11 +1,12 @@
 # TAG = lh
 	.text
-	
+	lui x11, 6
 	lui x14 , 1
 	lh x31, 0(x14)
 	
 	
 	# max_cycle 50
 	# pout_start
-	# 00000037
+	# 000065B7
 	# pout_end
+
